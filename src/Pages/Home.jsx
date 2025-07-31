4643fe086b2929c0bb5dad41eabdd077eca097a2
Jean Melgar <jean041519@gmail.com>
2025-07-30 19:08:26 -0500

    se soluciono la colision del badge del home con el nav

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -159,7 +159,7 @@ const Home = () => {
   };
 
   return (
-    <div className="min-h-screen bg-[#030014] overflow-hidden px-[5%] sm:px-[5%] lg:px-[10%] " id="Home">
+    <div className="min-h-screen bg-[#030014] overflow-hidden px-[5%] sm:px-[5%] lg:px-[10%] pt-16" id="Home">
       <div className={`relative z-10 transition-all duration-1000 ${isLoaded ? "opacity-100" : "opacity-0"}`}>
         <div className="container mx-auto  min-h-screen ">
           <div className="flex flex-col lg:flex-row items-center justify-center h-screen md:justify-between gap-0 sm:gap-12 lg:gap-20">
